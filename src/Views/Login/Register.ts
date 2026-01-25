718bf034ddb83848627e0fa0581257ee4243cfca
Tim Lin <lintim03@gmail.com>
2026-01-25 00:47:27 -0600

    Format Register Page

--- src/Views/Login/Register.ts
+++ src/Views/Login/Register.ts
@@ -10,13 +10,13 @@ export class Register implements View{
     RegisterWarning?:PopUpBox;
 
     constructor(onBack:BasicCallback){
-        const prompt = new Label(         {x:0, y: Sizes.text.title}, {x:640, y: 150}, "Register");
-        const backPanel = new Panel(      {x: 640, y:500}, GetPositionFromCenter({x:640, y:320}, {x: 640, y:500}));
-        const username = new TextBox(     {x:400, y:40},   GetPositionFromCenter({x:640, y:250}, {x:400, y:40}));
-        const password = new TextBox(     {x:400, y:40},   GetPositionFromCenter({x:640, y:320}, {x:400, y:40}));
-        const passwordRepeat = new TextBox(     {x:400, y:40},   GetPositionFromCenter({x:640, y:320}, {x:400, y:40}));
-        const backButton = new Button(   {x:150, y:60},  GetPositionFromCenter({x:540, y:400}, {x:150, y:60}), "Back", () => onBack());
-        const registerButton = new Button(   {x:150, y:60},  GetPositionFromCenter({x:740, y:400}, {x:150, y:60}), "Register", () => this.OnRegister());
+        const prompt = new Label(          {x:0, y: Sizes.text.title}, {x:640, y: 180}, "Register");
+        const backPanel = new Panel(       {x: 640, y:400}, GetPositionFromCenter({x:640, y:320}, {x: 640, y:400}));
+        const username = new TextBox(      {x:400, y:40},   GetPositionFromCenter({x:640, y:250}, {x:400, y:40}));
+        const password = new TextBox(      {x:400, y:40},   GetPositionFromCenter({x:640, y:320}, {x:400, y:40}));
+        const passwordRepeat = new TextBox({x:400, y:40},   GetPositionFromCenter({x:640, y:390}, {x:400, y:40}));
+        const backButton = new Button(     {x:175, y:40},  GetPositionFromCenter({x:540, y:475}, {x:175, y:60}), "Back", () => onBack());
+        const registerButton = new Button( {x:175, y:40},  GetPositionFromCenter({x:740, y:475}, {x:175, y:60}), "Register", () => this.OnRegister());
 
         this.Children.push(backPanel, prompt, username, password, passwordRepeat, backButton, registerButton);
     }
@@ -25,6 +25,8 @@ export class Register implements View{
         this.RegisterWarning = new PopUpBox("User Authenticate has\nnot been implemented yet.",
             {callBack: () => this.OnRegisterOk(), text: "Ok"}
         );
+
+        this.Children.push(this.RegisterWarning);
     }
 
     private OnRegisterOk(){

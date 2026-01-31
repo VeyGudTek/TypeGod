89fa57b67dc4e7f1765670d0d835f8d305745cb7
Tim Lin <lintim03@gmail.com>
2026-01-31 17:09:46 -0600

    Create and Draw Cutscene container

---
+++ src/Views/CutScene/CutSceneContainer.ts
@@ -0,0 +1,12 @@
+import { GetPositionFromCenter } from "@Functions/PositionConversion";
+import { BaseView, Panel } from "@Views/Shared";
+
+export class CutSceneContainer extends BaseView{
+    constructor(){
+        super();
+
+        const textPanel = new Panel({x: 1000, y: 200}, GetPositionFromCenter({x:640, y: 320}, {x: 1000, y: 200}));
+
+        this.Children.push(textPanel);
+    }
+}
--- src/Views/Main.ts
+++ src/Views/Main.ts
@@ -3,22 +3,23 @@ import { windowProvider } from "@Services/.";
 import { BaseTransformView } from "@Views/Shared";
 import { LoginContainer } from "@Views/Login";
 import { Colors, Sizes } from "@Static/.";
+import { CutSceneContainer } from "@Views/CutScene";
 
 export class Main extends BaseTransformView{
-    LoginContainer: LoginContainer;
+    LoginContainer?: LoginContainer;
+    CutSceneContainer?: CutSceneContainer
 
     constructor(){
         super(windowProvider.WindowSize, {x:0, y:0});
 
-        this.LoginContainer = new LoginContainer();
-        this.Children.push(this.LoginContainer);
+        //this.LoginContainer = new LoginContainer();
+        //this.Children.push(this.LoginContainer);
+
+        this.CutSceneContainer = new CutSceneContainer();
+        this.Children.push(this.CutSceneContainer);
     }
 
-    OnUpdate(){
-        this.ClearScreen();
-    }
-
-    ClearScreen(){
+    Render(){
         DrawRect(this.Position, this.Size, Colors.background, Colors.border.base, Sizes.border.base);
     }
 }

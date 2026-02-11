28dcb2a6871da2dc470b93630da047c31f7e23d8
Tim Lin <lintim03@gmail.com>
2026-02-11 00:09:08 -0600

    Remove reference to NextPageFunction on Dialogue.ts

--- src/Views/CutScene/Dialogue.ts
+++ src/Views/CutScene/Dialogue.ts
@@ -5,21 +5,15 @@ import type { BasicCallback } from "@Models/Callbacks.type";
 
 export class Dialogue extends BaseHoverView{
     TextDialogue:DialogueText;
-    NextPage: BasicCallback;
 
     constructor(nextPage:BasicCallback, initialText: string, size:Vector2, position:Vector2){
         super(size, position);
-        this.NextPage = nextPage;
 
         const textPanel = new Panel(size, position);
-        this.TextDialogue = new DialogueText(() => this.OnDialogueFinish(), {x: size.x * .8, y: size.y * .7}, position);
+        this.TextDialogue = new DialogueText(() => nextPage(), {x: size.x * .8, y: size.y * .7}, position);
         this.TextDialogue.SetDialogueText(initialText);
 
         this.Children.push(textPanel, this.TextDialogue);
-    }
-
-    OnDialogueFinish(){
-        this.NextPage();
     }
 
     OnNextPage(nextText:string){

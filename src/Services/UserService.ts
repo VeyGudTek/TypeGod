236ab2ae55fa161b38e2743b6cf16ebd69197ab8
Tim Lin <lintim03@gmail.com>
2026-03-14 14:30:43 -0500

    Show continue or not based on existing Data

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -1,8 +1,49 @@
-import type { User } from "@Models/.";
+import type { CharacterIndex, User, CharacterData } from "@Models/.";
 import { defaultUserData } from "@Static/.";
 
 class UserService{
+    private readonly storageIndex = "userData";
     private User:User = defaultUserData;
+    private ExistingData:string | null;
+
+    constructor(){
+        this.ExistingData = window.localStorage.getItem(this.storageIndex);
+    }
+
+    CheckExisting(){
+        return this.ExistingData !== null && !this.CheckMalformed();
+    }
+
+    LoadExisting(){
+        if (this.CheckExisting()){
+            this.User = JSON.parse(this.ExistingData as string);
+        }
+        else{
+            window.localStorage.setItem(this.storageIndex, JSON.stringify(this.User))
+        }
+    }
+
+    private CheckMalformed(){
+        try{
+            if (this.ExistingData !== null){
+                this.User = JSON.parse(this.ExistingData);
+
+                Object.keys(defaultUserData).forEach((characterIndex) => {
+                    const index = characterIndex as CharacterIndex;
+                    Object.keys(defaultUserData[index]).forEach((characterProperty) => {
+                        const property = this.User[index][characterProperty as keyof CharacterData];
+                        if (typeof property !== "number"){
+                            throw new Error();
+                        }
+                    });
+                });
+            }
+        }
+        catch{
+            return true;
+        }
+        return false;
+    }
 
     GetUserData(){
         return structuredClone(this.User);
--- src/Views/Start/Start.ts
+++ src/Views/Start/Start.ts
@@ -3,18 +3,41 @@ import splashLeftSource from "@Assets/Images/splashLeft.png";
 import splashRightSource from "@Assets/Images/splashRight.png";
 import { Sizes } from "@Static/.";
 import type { BasicCallback } from "@Models/Callbacks.type";
+import { userService } from "@Services/UserService";
 
 export class Start extends BaseView{
+    OnStart:BasicCallback;
+
     constructor(onStart:BasicCallback){
         super();
+        this.OnStart = onStart;
 
         const splashLeft = new Picture(splashLeftSource,   {x:1344, y:2226}, 1.25, {x:.17, y:.65});
         const splashRight = new Picture(splashRightSource, {x:1394, y:2595}, 1.5, {x:.85, y:.65});
-        
+
         const backPanel = new Panel(    {x:.35, y:.6},              {x:.5, y:.5});
         const prompt = new Label(       {x:0, y: Sizes.text.title}, {x:.5, y:.3}, "Type God");
-        const newGame = new Button(     {x:.18, y:.07},             {x:.5, y:.55}, "New Game", () => onStart());
 
-        this.Children.push(splashLeft, splashRight, backPanel, prompt, newGame);
+        this.Children.push(splashLeft, splashRight, backPanel, prompt);
+
+        this.CreateButtons();
+    }
+
+    private CreateButtons(){
+        const hasData = userService.CheckExisting();
+        const newButtonY = hasData ? .47 : .55
+
+        const newGame = new Button(         {x:.18, y:.07}, {x:.5, y:newButtonY}, "New Game", () => this.StartGame());
+        this.Children.push(newGame);
+
+        if (hasData){
+            const continueGame = new Button({x:.25, y:.07}, {x:.5, y:.65}, "Continue Game", () => this.StartGame());
+            this.Children.push(continueGame);
+        }
+    }
+
+    private StartGame(){
+        userService.LoadExisting();
+        this.OnStart();
     }
 }

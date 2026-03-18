d7c097e9edec24ea50e707429136e256b58b8579
Tim Lin <lintim03@gmail.com>
2026-03-17 22:50:30 -0500

    Add name to character view

--- src/Views/Home/CharacterUpgrade.ts
+++ src/Views/Home/CharacterUpgrade.ts
@@ -1,15 +1,17 @@
 import type { CharacterIndex, BasicCallback } from "@Models/.";
 import { userService } from "@Services/UserService";
-import { BaseView, Button } from "@Views/.";
+import { BaseView, Button, Label } from "@Views/.";
 import { CharacterUpgradeStats } from "./CharacterUpgradeStats";
+import { Sizes } from "@Static/Styles";
 
 export class CharacterUpgrade extends BaseView{
     constructor(characterIndex:CharacterIndex, onBack:BasicCallback){
         super();
 
         const backButton = new Button({x:.1, y:.05}, {x:.1, y:.09}, "Back", () => onBack());
+        const name = new Label({x:.1, y: Sizes.text.title}, {x:.5, y:.1}, characterIndex);
         const characterStats = new CharacterUpgradeStats(userService.GetUserData()[characterIndex]);
 
-        this.Children.push(backButton, characterStats);
+        this.Children.push(backButton, name, characterStats);
     }
 }

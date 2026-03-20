8e476fd12a5e27703dc5bfb39b3bb86878b6c455
Tim Lin <lintim03@gmail.com>
2026-03-20 00:57:46 -0500

    Create Labels for time and characters

--- src/Views/Results/Results.ts
+++ src/Views/Results/Results.ts
@@ -9,8 +9,13 @@ export class Results extends BaseView{
         const backPanel = new Panel({x: .4, y: .8}, {x:.25, y:.5});
         const title = new Label({x: .4, y: Sizes.text.title}, {x:.25, y:.2}, "Results", "center");
 
+        const charactersText = `Characters: ${characters}`;
+        const timeText = `Time: ${time}`;
+        const charactersLabel = new Label({x: .4, y: Sizes.text.base}, {x:.1, y:.3}, charactersText, "start");
+        const timeLabel =       new Label({x: .4, y: Sizes.text.base}, {x:.1, y:.4}, timeText, "start");
+
         const characterResults = new CharacterResults(experience);
 
-        this.Children.push(backPanel, title, characterResults);
+        this.Children.push(backPanel, title, charactersLabel, timeLabel, characterResults);
     }
 }

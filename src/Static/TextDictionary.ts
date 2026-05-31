b5657306195210a02cb8911e5ec20f6fdc7dc5de
Tim Lin <lintim03@gmail.com>
2026-05-31 16:09:36 -0500

    Add Text Images and Dictionary

---
+++ src/Static/TextDictionary.ts
@@ -0,0 +1,119 @@
+import Period from "@Assets/Images/Text/Period.jpg";
+import Comma from "@Assets/Images/Text/Comma.jpg";
+import Question from "@Assets/Images/Text/Question.jpg";
+import Space from "@Assets/Images/Text/Space.jpg";
+import Apostrophe from "@Assets/Images/Text/Apostrophe.jpg";
+
+import A from "@Assets/Images/Text/A.jpg";
+import a from "@Assets/Images/Text/A_Low.jpg";
+import B from "@Assets/Images/Text/B.jpg";
+import b from "@Assets/Images/Text/B_Low.jpg";
+import C from "@Assets/Images/Text/C.jpg";
+import c from "@Assets/Images/Text/C_Low.jpg";
+import D from "@Assets/Images/Text/D.jpg";
+import d from "@Assets/Images/Text/D_Low.jpg";
+import E from "@Assets/Images/Text/E.jpg";
+import e from "@Assets/Images/Text/E_Low.jpg";
+import F from "@Assets/Images/Text/F.jpg";
+import f from "@Assets/Images/Text/F_Low.jpg";
+import G from "@Assets/Images/Text/G.jpg";
+import g from "@Assets/Images/Text/G_Low.jpg";
+import H from "@Assets/Images/Text/H.jpg";
+import h from "@Assets/Images/Text/H_Low.jpg";
+import I from "@Assets/Images/Text/I.jpg";
+import i from "@Assets/Images/Text/I_Low.jpg";
+import J from "@Assets/Images/Text/J.jpg";
+import j from "@Assets/Images/Text/J_Low.jpg";
+import K from "@Assets/Images/Text/K.jpg";
+import k from "@Assets/Images/Text/K_Low.jpg";
+import L from "@Assets/Images/Text/L.jpg";
+import l from "@Assets/Images/Text/L_Low.jpg";
+import M from "@Assets/Images/Text/M.jpg";
+import m from "@Assets/Images/Text/M_Low.jpg";
+import N from "@Assets/Images/Text/N.jpg";
+import n from "@Assets/Images/Text/N_Low.jpg";
+import O from "@Assets/Images/Text/O.jpg";
+import o from "@Assets/Images/Text/O_Low.jpg";
+import P from "@Assets/Images/Text/P.jpg";
+import p from "@Assets/Images/Text/P_Low.jpg";
+import Q from "@Assets/Images/Text/Q.jpg";
+import q from "@Assets/Images/Text/Q_Low.jpg";
+import R from "@Assets/Images/Text/R.jpg";
+import r from "@Assets/Images/Text/R_Low.jpg";
+import S from "@Assets/Images/Text/S.jpg";
+import s from "@Assets/Images/Text/S_Low.jpg";
+import T from "@Assets/Images/Text/T.jpg";
+import t from "@Assets/Images/Text/T_Low.jpg";
+import U from "@Assets/Images/Text/U.jpg";
+import u from "@Assets/Images/Text/U_Low.jpg";
+import V from "@Assets/Images/Text/V.jpg";
+import v from "@Assets/Images/Text/V_Low.jpg";
+import W from "@Assets/Images/Text/W.jpg";
+import w from "@Assets/Images/Text/W_Low.jpg";
+import X from "@Assets/Images/Text/X.jpg";
+import x from "@Assets/Images/Text/X_Low.jpg";
+import Y from "@Assets/Images/Text/Y.jpg";
+import y from "@Assets/Images/Text/Y_Low.jpg";
+import Z from "@Assets/Images/Text/Z.jpg";
+import z from "@Assets/Images/Text/Z_Low.jpg";
+
+export const textDictionary: Record<string, string> = {
+    ".": Period,
+    ",": Comma,
+    "?": Question,
+    " ": Space,
+    "'": Apostrophe,
+
+    "A": A,
+    "a": a,
+    "B": B,
+    "b": b,
+    "C": C,
+    "c": c,
+    "D": D,
+    "d": d,
+    "E": E,
+    "e": e,
+    "F": F,
+    "f": f,
+    "G": G,
+    "g": g,
+    "H": H,
+    "h": h,
+    "I": I,
+    "i": i,
+    "J": J,
+    "j": j,
+    "K": K,
+    "k": k,
+    "L": L,
+    "l": l,
+    "M": M,
+    "m": m,
+    "N": N,
+    "n": n,
+    "O": O,
+    "o": o,
+    "P": P,
+    "p": p,
+    "Q": Q,
+    "q": q,
+    "R": R,
+    "r": r,
+    "S": S,
+    "s": s,
+    "T": T,
+    "t": t,
+    "U": U,
+    "u": u,
+    "V": V,
+    "v": v,
+    "W": W,
+    "w": w,
+    "X": X,
+    "x": x,
+    "Y": Y,
+    "y": y,
+    "Z": Z,
+    "z": z
+};

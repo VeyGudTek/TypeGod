14140fe9ae6c23bd41170f8b52f9d859bec1a4a7
Tim Lin <lintim03@gmail.com>
2026-06-02 23:05:35 -0500

    Wrap Root Scenes with image loader

--- src/Views/Main.ts
+++ src/Views/Main.ts
@@ -1,16 +1,10 @@
 import { LoadLevelOrHome, DrawRect, LoadLevelOrCutscene, LoadCutsceneOrHome } from "@Functions/.";
 import { windowProvider } from "@Services/.";
-import { BaseTransformView, CutScene, GameManager, Start, HomeContainer, Results } from "@Views/.";
-import { Colors, Sizes, scriptDictionary, stageStartToScriptDictionary, stageEndToScriptDictionary, testScript } from "@Static/.";
+import { BaseTransformView, CutScene, GameManager, HomeContainer, Results, LoaderWrapper } from "@Views/.";
+import { Colors, Sizes, scriptDictionary, stageStartToScriptDictionary, stageEndToScriptDictionary } from "@Static/.";
 import type { ScriptIndex, StageIndex } from "@Models/.";
 
 export class Main extends BaseTransformView{
-    private Start?: Start;
-    private HomeContainer?: HomeContainer;
-    private CutScene?: CutScene
-    private GameManager?: GameManager
-    private Results?: Results;
-
     constructor(){
         super(windowProvider.WindowSize, {x:0, y:0});
 
@@ -29,37 +23,41 @@ export class Main extends BaseTransformView{
 
     private LoadCutscene(index:ScriptIndex){
         this.Children = [];
-        this.CutScene = new CutScene(
+        const cutscene = new CutScene(
             scriptDictionary[index], 
             () => LoadLevelOrHome(
                 index, 
                 () => this.LoadHome(), 
                 (stageIndex) => this.LoadLevel(stageIndex)
         ));
-        this.Children.push(this.CutScene);
+        
+        this.Children.push(new LoaderWrapper("cutscene", cutscene, index));
     }
 
     private LoadHome(){
         this.Children = [];
-        this.HomeContainer = new HomeContainer((stageIndex) => 
+        const home = new HomeContainer((stageIndex) => 
             LoadLevelOrCutscene(
                 stageIndex, 
                 () => this.LoadCutscene(stageStartToScriptDictionary[stageIndex]), 
                 () => this.LoadLevel(stageIndex)
         ));
-        this.Children.push(this.HomeContainer);
+
+        this.Children.push(new LoaderWrapper("home", home));
     }
 
     private LoadLevel(stageIndex:StageIndex){
         this.Children = [];
-        this.GameManager = new GameManager(stageIndex, (exp, char, time) => this.LoadResults(exp, char, time, stageIndex));
-        this.Children.push(this.GameManager);
+        const level = new GameManager(stageIndex, (exp, char, time) => this.LoadResults(exp, char, time, stageIndex));
+
+        this.Children.push(new LoaderWrapper("level", level));
     }
 
     private LoadResults(exp:number, char:number, time:number, stageIndex:StageIndex){
         this.Children = [];
-        this.Results = new Results(exp, char, time, () => this.LoadCutscene(stageEndToScriptDictionary[stageIndex]));
-        this.Children.push(this.Results);
+        const results = new Results(exp, char, time, () => this.LoadCutscene(stageEndToScriptDictionary[stageIndex]));
+
+        this.Children.push(new LoaderWrapper("results", results));
     }
 
     Render(){
